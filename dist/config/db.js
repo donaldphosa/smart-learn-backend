8198c19a4afaa2fdce17d715dbb338b4db18bb43
donald phosa <86829073+donaldphosa@users.noreply.github.com>
2024-11-13 15:42:59 +0200

    Add files via upload

--- dist/config/db.js
+++ dist/config/db.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.MONGO_URI = void 0;
-exports.MONGO_URI = 'mongodb+srv://donaldbossd:%40DONALDp2017@smart-learn.jhwoc.mongodb.net/?retryWrites=true&w=majority&appName=smart-learn';
+exports.MONGO_URI = 'mongodb+srv://donaldbossd:%40DONALDp2017@smart-learn.jhwoc.mongodb.net/smart-learn-db?retryWrites=true&w=majority&appName=smart-learn&ssl=true';
